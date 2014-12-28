63b4a764de9886006b841e988dee7287d6af43ef
Qiang <koqizhao@outlook.com>
2014-12-28 22:37:58 +0800

    add float round

--- src/extensions/mathext/float.go
+++ src/extensions/mathext/float.go
@@ -1,6 +1,7 @@
 package mathext
 
 import "math"
+import "fmt"
 
 const DEFAULT_FLOAT_ACCURACY int = 8
 const _DEFAULT_FLOAT_ACCURACY int = 8
@@ -12,6 +13,25 @@ func AreEqualFloat(x, y float64) bool {
 
 func AreEqualFloatWithAccuracy(x, y float64, accuracy int) bool {
     return math.Abs(x - y) < calculateMarginOfError(accuracy)
+}
+
+func Round(x float64) int64 {
+    whole, fraction := math.Modf(x)
+    if whole < math.MinInt64 || whole > math.MaxInt64 {
+        panic(fmt.Sprintf("%g is out of the int64 range", x))
+    }
+    if math.Abs(fraction) >= 0.5 {
+        if x >= 0 {
+            whole++
+        } else {
+            whole--
+        }
+    }
+    return int64(whole)
+}
+
+func RoundFloat32(x float32) int32 {
+    return int32(Round(float64(x)))
 }
 
 func calculateMarginOfError(accuracy int) float64 {
--- src/extensions/mathext/float_test.go
+++ src/extensions/mathext/float_test.go
@@ -8,6 +8,11 @@ const _TEST_DOUBLE_INCREMENT = 0.2
 const _TEST_ITERATION_TIMES = 1000
 const _IGNORE_INT_VALUE = int(math.MinInt32)
 
+var _float64Values = []float64 { 1, 1.5, 1.1, 1.0, 1.7, 2.0, 0, -1, -1.1, -1.5, -1.7, -2.0 }
+var _roundedInt64Values = []int64 { 1, 2, 1, 1, 2, 2, 0, -1, -1, -2, -2, -2}
+var _float32Values = []float32 { 1, 1.5, 1.1, 1.0, 1.7, 2.0, 0, -1, -1.1, -1.5, -1.7, -2.0 }
+var _roundedInt32Values = []int32 { 1, 2, 1, 1, 2, 2, 0, -1, -1, -2, -2, -2}
+
 func TestAreEqualFloat(t *testing.T) {
     testAreEqualFloat(t, _IGNORE_INT_VALUE)
 }
@@ -16,6 +21,24 @@ func TestAreEqualFloatWithAccuracy(t *testing.T) {
     accuracyList := []int { -1, 0, _DEFAULT_FLOAT_ACCURACY -1, _DEFAULT_FLOAT_ACCURACY }
     for _, v := range accuracyList {
         testAreEqualFloat(t, v)
+    }
+}
+
+func TestRound(t *testing.T) {
+    for i, l := 0, len(_float64Values); i < l; i++ {
+        if Round(_float64Values[i]) != _roundedInt64Values[i] {
+            t.Error("Float", _float64Values[i], "Int", _roundedInt64Values[i], "Expected", "Equal")
+            return;
+        }
+    }
+}
+
+func TestRoundFloat32(t *testing.T) {
+    for i, l := 0, len(_float32Values); i < l; i++ {
+        if RoundFloat32(_float32Values[i]) != _roundedInt32Values[i] {
+            t.Error("Float", _float32Values[i], "Int", _roundedInt32Values[i], "Expected", "Equal")
+            return;
+        }
     }
 }
 
