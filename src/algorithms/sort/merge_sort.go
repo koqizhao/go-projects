7a06e9a3293efac53aee41603e15b59e1678e2e2
Qiang <koqizhao@outlook.com>
2014-12-31 21:34:36 +0800

    add down-top merge sort

--- src/algorithms/sort/merge_sort.go
+++ src/algorithms/sort/merge_sort.go
@@ -8,6 +8,7 @@ func MergeSort(arr *[]int) {
 	l := len(*arr)
 	tempArr := make([]int, l)
 	mergeSort(arr, 0, l, &tempArr)
+	// mergeSortDownTop(arr, &tempArr)
 }
 
 func mergeSort(arr *[]int, start, end int, tempArr *[]int) {
@@ -31,5 +32,32 @@ func mergeSort(arr *[]int, start, end int, tempArr *[]int) {
 
 	for i := start; i < end; i++ {
 		(*arr)[i] = (*tempArr)[i]
+	}
+}
+
+func mergeSortDownTop(arr *[]int, tempArr *[]int) {
+	for i, l := 1, len(*arr); i < l; i *= 2 {
+		for j, span := 0, i * 2; j < l; j += span {
+			middle, end := j + i, j + span
+			if middle >= l {
+				continue
+			}
+			if end >= l {
+				end = l
+			}
+			for k, s1, s2 := j, j, middle; k < end; k++ {
+				if s1 < middle && (s2 >= end || (*arr)[s1] <= (*arr)[s2]) {
+					(*tempArr)[k] = (*arr)[s1]
+					s1++
+				} else {
+					(*tempArr)[k] = (*arr)[s2]
+					s2++
+				}
+			}
+
+			for k := j; k < end; k ++ {
+				(*arr)[k] = (*tempArr)[k]
+			}
+		}
 	}
 }
