18329665f169003e970db162a7fda59877432d52
q_zhao <q_zhao@Ctrip.com>
2015-01-15 19:18:49 +0800

    refine code

--- src/extensions/mathext/float.go
+++ src/extensions/mathext/float.go
@@ -3,76 +3,75 @@ package mathext
 import "math"
 import "fmt"
 
-const DEFAULT_FLOAT_ACCURACY int = 8
 const _DEFAULT_FLOAT_ACCURACY int = 8
 const _MAX_FLOAT_ACCURACY int = 128
 
 func AreEqualFloat(x, y float64) bool {
-    return AreEqualFloatWithAccuracy(x, y, _DEFAULT_FLOAT_ACCURACY)
+	return AreEqualFloatWithAccuracy(x, y, _DEFAULT_FLOAT_ACCURACY)
 }
 
 func AreEqualFloatWithAccuracy(x, y float64, accuracy int) bool {
-    return math.Abs(x - y) < calculateMarginOfError(accuracy)
+	return math.Abs(x-y) < calculateMarginOfError(accuracy)
 }
 
 func Round(x float64) int64 {
-    whole, fraction := math.Modf(x)
-    if whole < math.MinInt64 || whole > math.MaxInt64 {
-        panic(fmt.Sprintf("%g is out of the int64 range", x))
-    }
-    if math.Abs(fraction) >= 0.5 {
-        if x >= 0 {
-            whole++
-        } else {
-            whole--
-        }
-    }
-    return int64(whole)
+	whole, fraction := math.Modf(x)
+	if whole < math.MinInt64 || whole > math.MaxInt64 {
+		panic(fmt.Sprintf("%g is out of the int64 range", x))
+	}
+	if math.Abs(fraction) >= 0.5 {
+		if x >= 0 {
+			whole++
+		} else {
+			whole--
+		}
+	}
+	return int64(whole)
 }
 
 func RoundFloat32(x float32) int32 {
-    return int32(Round(float64(x)))
+	return int32(Round(float64(x)))
 }
 
 func Sum(arr []float64) float64 {
-    result := 0.0
-    for i, l := 0, len(arr); i < l; i++ {
-        result += arr[i]
-    }
-    return result;
+	result := 0.0
+	for i, l := 0, len(arr); i < l; i++ {
+		result += arr[i]
+	}
+	return result
 }
 
 func Avg(arr []float64) float64 {
-    if l := len(arr); l == 0 {
-        return 0
-    } else {
-        return Sum(arr) / float64(l)
-    }
+	if l := len(arr); l == 0 {
+		return 0
+	} else {
+		return Sum(arr) / float64(l)
+	}
 }
 
 func Median(arr []float64) float64 {
-    l := len(arr);
-    if l == 0 {
-        return 0
-    }
+	l := len(arr)
+	if l == 0 {
+		return 0
+	}
 
-    result := arr[ l / 2]
-    if l % 2 == 0 {
-        result = (result + arr[ l / 2 - 1]) / 2
-    }
-    return result
+	result := arr[l/2]
+	if l%2 == 0 {
+		result = (result + arr[l/2-1]) / 2
+	}
+	return result
 }
 
 func calculateMarginOfError(accuracy int) float64 {
-    if accuracy <= 0 {
-        accuracy = _DEFAULT_FLOAT_ACCURACY
-    } else if accuracy > _MAX_FLOAT_ACCURACY {
-        accuracy = _MAX_FLOAT_ACCURACY
-    }
+	if accuracy <= 0 {
+		accuracy = _DEFAULT_FLOAT_ACCURACY
+	} else if accuracy > _MAX_FLOAT_ACCURACY {
+		accuracy = _MAX_FLOAT_ACCURACY
+	}
 
-    result := 1.0
-    for i := 0; i < accuracy; i++ {
-        result /= 10
-    }
-    return result
+	result := 1.0
+	for i := 0; i < accuracy; i++ {
+		result /= 10
+	}
+	return result
 }
