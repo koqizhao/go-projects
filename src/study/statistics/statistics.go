67e3802bc5423168706a9c34b8111173bd6691f4
koqizhao <koqizhao@outlook.com>
2022-05-08 07:42:51 +0800

    refine code

--- src/study/statistics/statistics.go
+++ src/study/statistics/statistics.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-    "sort"
-    "extensions/mathext"
-    )
+	"extensions/mathext"
+	"sort"
+)
 
 type statistics struct {
-    numbers []float64
-    mean float64
-    median float64
+	numbers []float64
+	mean    float64
+	median  float64
 }
 
 func NewStatistics(numbers []float64) *statistics {
-    var stats statistics
-    stats.numbers = numbers
-    if l := len(numbers); l > 0 {
-        sort.Float64s(numbers)
-        stats.mean = mathext.Avg(numbers)
-        stats.median = mathext.Median(numbers)
-    }
-    return &stats
+	var stats statistics
+	stats.numbers = numbers
+	if l := len(numbers); l > 0 {
+		sort.Float64s(numbers)
+		stats.mean = mathext.Avg(numbers...)
+		stats.median = mathext.Median(numbers...)
+	}
+	return &stats
 }
